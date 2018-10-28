eba97226bfe2bb449c17e91fbf8727ca3d55cec5
Michal Muszynski <m.muszy@gmail.com>
2018-10-28 16:01:30 +0100

    Fixed menu transition

--- projects/technical-documentation/main.js
+++ projects/technical-documentation/main.js
@@ -6,3 +6,13 @@ button.addEventListener('click', showIt);
 function showIt() {
   navigationList.classList.toggle('open')
 }
+
+var resizeTimer;
+window.onresize = () => {
+  navigationList.classList.add('transition-off');
+  
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => navigationList.classList.remove('transition-off'), 250);
+};
+
+
