38924e163134777471b5973b27d5bb06b8300e5f
Michal Muszynski <m.muszy@gmail.com>
2018-10-28 20:08:26 +0100

    Scroll behavior functino

--- projects/technical-documentation/main.js
+++ projects/technical-documentation/main.js
@@ -1,10 +1,34 @@
 const button = document.querySelector('.menu-button');
 const navigationList = document.querySelector('.nav-list');
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
 
 button.addEventListener('click', showIt);
 
+anchorLinks.forEach(anchor => anchor.addEventListener('click', anchorScroll))
+
 function showIt() {
   navigationList.classList.toggle('open')
+}
+
+function anchorScroll(e) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+
+  e.preventDefault();
+  const targetID = this.getAttribute("href");
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+
+  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+
+  const checkIfDone = setInterval(function() {
+      const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+      if (distanceToTop(targetAnchor) === 0 || atBottom) {
+          targetAnchor.tabIndex = "-1";
+          window.history.pushState("", "", targetID);
+          clearInterval(checkIfDone);
+      }
+  }, 100);
 }
 
 var resizeTimer;
