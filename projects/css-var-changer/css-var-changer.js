e9cc3312ea3708eafadd3aecd4f252e38a670f5a
Michal Muszynski <m.muszy@gmail.com>
2018-12-22 19:16:56 +0100

    JS codebase

--- projects/css-var-changer/css-var-changer.js
+++ projects/css-var-changer/css-var-changer.js
@@ -1,1 +1,8 @@
+const inputs = Array.from(document.querySelectorAll('input'));
 
+function handleChange() {
+  console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleChange));
+inputs.forEach(input => input.addEventListener('mousemove', handleChange));
