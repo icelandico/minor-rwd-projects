4e2254209dd602b5461342c4b405147bc9871029
Michal Muszynski <m.muszy@gmail.com>
2018-12-22 19:46:45 +0100

    Handle properties changes

--- projects/css-var-changer/css-var-changer.js
+++ projects/css-var-changer/css-var-changer.js
@@ -4,8 +4,12 @@ const backgroundImg = document.querySelector('.background-image');
 function handleChange() {
   if (this.type === 'color') {
     backgroundImg.style.setProperty('--base', this.value);
-  } else if (this.type === 'range') {
-    backgroundImg.style.setProperty('--opacity', this.value / 100 )
+  } else if (this.id === 'opacity') {
+    backgroundImg.style.setProperty('--opacity', this.value / 100 );
+  } else if (this.id === 'spacing') {
+    backgroundImg.style.setProperty('--spacing', this.value + 'px');
+  } else if (this.id === 'blur') {
+    backgroundImg.style.setProperty('--blur', this.value + 'px');
   }
 }
 
