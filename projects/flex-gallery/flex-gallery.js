c778e6e312b5a5deac3c2b49e6e7eee78f9e2b35
Michal Muszynski <m.muszy@gmail.com>
2018-12-27 14:43:30 +0100

    Finished animation

--- projects/flex-gallery/flex-gallery.js
+++ projects/flex-gallery/flex-gallery.js
@@ -4,4 +4,11 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
+function toggleActive(event) {
+  if (event.propertyName.includes('flex')) {
+  this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
